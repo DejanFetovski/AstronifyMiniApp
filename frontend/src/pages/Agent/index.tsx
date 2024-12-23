450c84c70e12b900645c25db0de256256de0f130
Spider <164912081+DanijelTodorov@users.noreply.github.com>
2024-12-24 08:23:14 +0900

    update agent page
    update agent page

--- frontend/src/pages/Agent/index.tsx
+++ frontend/src/pages/Agent/index.tsx
@@ -1,5 +1,20 @@
+import AskInput from "../../components/AskInput";
+import BackIcon from "../../svgs/BackIcon";
+
 const Agent = () => {
-  return <div></div>;
+  return (
+    <div className="relative min-h-screen flex flex-col justify-end gap-[40px] px-6 pt-[30px] pb-[28px]">
+      <img
+        src="assets/images/diagram.png"
+        className="absolute top-0 right-0"
+      ></img>
+      <div className="absolute top-10 left-8">
+        <BackIcon />
+      </div>
+
+      <AskInput />
+    </div>
+  );
 };
 
 export default Agent;
